491875e3d398af771e8437bf8272a35d8bc17c6d
Adrian de la Rosa <adelarosabretin@coursera.org>
2024-10-18 12:29:14 -0400

    tests: add more use cases

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,8 +1,7 @@
 import { readFileSync } from 'fs';
 import { describe, expect, it } from 'vitest';
 
-// @ts-expect-error - Required file extension ?
-import parse from '../index';
+import parse from '../index.js';
 
 function readFile(path: string): string {
   const filename = require.resolve(path);
@@ -11,24 +10,32 @@ function readFile(path: string): string {
 }
 
 const basic = readFile('./grammar/basic.pgn');
+const benko = readFile('./grammar/benko.pgn');
 const checkmate = readFile('./grammar/checkmate.pgn');
 const comment = readFile('./grammar/comment.pgn');
 const comments = readFile('./grammar/comments.pgn');
+const games32 = readFile('./grammar/games32.pgn');
+const lichess = readFile('./grammar/lichess.pgn');
+const long = readFile('./grammar/long.pgn');
 const multiple = readFile('./grammar/multiple-game.pgn');
 const promotion = readFile('./grammar/promotion.pgn');
-const variants = readFile('./grammar/variants.pgn');
-const long = readFile('./grammar/long.pgn');
 const single = readFile('./grammar/single.pgn');
+const twic = readFile('./grammar/twic.pgn');
+const variants = readFile('./grammar/variants.pgn');
 
 const tests = {
   basic,
+  benko,
   checkmate,
   comment,
   comments,
+  games32,
+  lichess,
   long,
   multiple,
   promotion,
   single,
+  twic,
   variants,
 };
 
