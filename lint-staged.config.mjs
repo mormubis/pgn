27ff8a1e8458d3a2addbb59defa5001198394b71
Adrian de la Rosa <adelarosabretin@coursera.org>
2024-11-05 18:55:54 +0100

    chore: better lint-staged config

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,5 @@
 export default {
   '*.(cjs|js|jsx|mjs|mts|ts|tsx)': ['eslint --fix --max-warnings 0'],
   '*.(css|js|json|jsx|md|mjs|mts|ts|tsx|yml|yaml)': ['prettier --write'],
+  '*.(ne|ts)': ['pnpm test'],
 };
