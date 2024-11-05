f3f4ab27b8419d25f93bf76cffe50e861725e883
Adrian de la Rosa <adelarosabretin@coursera.org>
2024-11-05 13:16:38 +0100

    feat: add benchmark

---
+++ src/__tests__/index.bench.ts
@@ -0,0 +1,48 @@
+import { readFileSync } from 'fs';
+import { bench, describe } from 'vitest';
+
+import parse from '../index.js';
+
+function readFile(path: string): string {
+  const filename = require.resolve(path);
+
+  return readFileSync(filename, 'utf8');
+}
+
+const basic = readFile('./grammar/basic.pgn');
+const benko = readFile('./grammar/benko.pgn');
+const checkmate = readFile('./grammar/checkmate.pgn');
+const comment = readFile('./grammar/comment.pgn');
+const comments = readFile('./grammar/comments.pgn');
+const games32 = readFile('./grammar/games32.pgn');
+const lichess = readFile('./grammar/lichess.pgn');
+const long = readFile('./grammar/long.pgn');
+const multiple = readFile('./grammar/multiple-game.pgn');
+const promotion = readFile('./grammar/promotion.pgn');
+const single = readFile('./grammar/single.pgn');
+const twic = readFile('./grammar/twic.pgn');
+const variants = readFile('./grammar/variants.pgn');
+
+const tests = {
+  basic,
+  benko,
+  checkmate,
+  comment,
+  comments,
+  games32,
+  lichess,
+  long,
+  multiple,
+  promotion,
+  single,
+  twic,
+  variants,
+};
+
+describe('PGN Parser', () => {
+  Object.entries(tests).forEach(([label, input]) => {
+    bench(label, () => {
+      parse(input);
+    });
+  });
+});
