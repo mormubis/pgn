20991db1124eabf974df2bb4244f89129cb4169a
Adrian de la Rosa <github@brick.email>
2026-03-18 21:42:31 +0100

    chore: remove coverage thresholds from vitest config

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -5,26 +5,6 @@ export default defineConfig({
     coverage: {
       exclude: ['**/__tests__/**', '**/*.bench.ts'],
       provider: 'v8',
-      thresholds: {
-        'src/parse.ts': {
-          branches: 85,
-          functions: 100,
-          lines: 98,
-          statements: 98,
-        },
-        'src/stream.ts': {
-          branches: 97,
-          functions: 100,
-          lines: 98,
-          statements: 98,
-        },
-        'src/stringify.ts': {
-          branches: 90,
-          functions: 100,
-          lines: 98,
-          statements: 98,
-        },
-      },
     },
   },
 });
