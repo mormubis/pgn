c0c2021707c0e5c80e11fee23efb1748a86beb2a
Adrian de la Rosa <github@brick.email>
2026-04-26 10:23:03 -0400

    chore: use explicit glob pattern in lint-staged config (#587)

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
-  '*': ['prettier --write --ignore-unknown'],
+  '*.(css|js|json|jsx|md|mjs|mts|ts|tsx|yml|yaml)': ['prettier --write'],
   '*.(js|jsx|mjs|mts|ts|tsx)': [
     'eslint --fix --max-warnings 0 --no-warn-ignored',
   ],
